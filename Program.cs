10e7768e20168da63cb48ef52844f486fe34405f
Evan <evan.j.ducas@gmail.com>
2025-01-02 12:55:34 -0500

    Added the command to submit a file path, in the future this command will run a macro inside the file!

--- Program.cs
+++ Program.cs
@@ -1,26 +1,38 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Program
 {
+
     public static void Main(string[] args)
     {
         if (args.Length == 0 || args[0] == "--help" || args[0] == "-h")
         {
             Console.WriteLine("Commands:");
             Console.WriteLine("--help or -h: Show this menu");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("{path\\to\\file}: Will run the file");
         }
-        string filePath = "C:\\Users\\evanj\\test.txt";
 
-        if (File.Exists(filePath))
-        {
-            string content = File.ReadAllText(filePath);
-            //Console.WriteLine("File Content:");
-            //Console.WriteLine(content);
+        else {
+
+            string filePath = args[0];
+            Console.WriteLine($"File Path: {filePath}");
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine("File Content:");
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"The file {filePath} does not exist.");
+            }
+
         }
-        else
-        {
-            Console.WriteLine($"The file {filePath} does not exist.");
-        }
+
+        Console.ReadLine();
+
     }
 
 }
